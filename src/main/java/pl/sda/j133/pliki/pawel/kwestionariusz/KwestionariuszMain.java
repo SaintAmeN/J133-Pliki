bacca0f408f3254e0436ccb277c287f6c366d739
Paweł Recław <pawel.reclaw@gmail.com>
2022-11-06 14:21:04 +0100

    1

--- src/main/java/pl/sda/j133/pliki/pawel/kwestionariusz/KwestionariuszMain.java
+++ src/main/java/pl/sda/j133/pliki/pawel/kwestionariusz/KwestionariuszMain.java
@@ -30,17 +30,31 @@ public class KwestionariuszMain {
             // jeśli wiek jest mniejszy od zera lub większy od 100
         } while (wiek <= 0 || wiek >= 100);
 
-        System.out.println("3. Podaj ogólne zadowolenie z kursu (1-10)?");
-        int zadowolenieOgolne = Integer.parseInt(scanner.nextLine());
+        int zadowolenieOgolne;
+        do {
+            System.out.println("3. Podaj ogólne zadowolenie z kursu (1-10)?");
+            zadowolenieOgolne = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("4. Czy poleciłbyś kurs znajomemu (tak/nie)?");
-        String czyPolecisz = scanner.nextLine();
+        } while (zadowolenieOgolne <= 0 || zadowolenieOgolne > 10);
 
-        System.out.println("5. Podaj ogólne zadowolenie z trenera (1-10)?");
-        int ocenaTrenera = Integer.parseInt(scanner.nextLine());
+        String czyPolecisz;
+        do {
+            System.out.println("4. Czy poleciłbyś kurs znajomemu (tak/nie)?");
+            czyPolecisz = scanner.nextLine();
+
+            // jeśli czy polecisz nie jest równe "tak" i czy polecisz nie jest równe "nie"
+        } while (!czyPolecisz.equals("tak") && !czyPolecisz.equals("nie"));
+
+        int ocenaTrenera;
+        do {
+            System.out.println("5. Podaj ogólne zadowolenie z trenera (1-10)?");
+            ocenaTrenera = Integer.parseInt(scanner.nextLine());
+        } while (ocenaTrenera <= 0 || ocenaTrenera > 10);
 
         System.out.println("6. Podziel się swoją opinią:");
         String opinia = scanner.nextLine();
+
+        opinia = opinia.replaceAll(";", "|");
 
         try {
             PrintWriter drukarkaTekstu = new PrintWriter(new FileWriter("ankieta_sda.txt", true));
