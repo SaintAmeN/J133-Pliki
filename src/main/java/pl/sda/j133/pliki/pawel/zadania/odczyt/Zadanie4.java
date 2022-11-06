3d7b1e0af526427dc3398b3b23f82b5c7fc5994e
Paweł Recław <pawel.reclaw@gmail.com>
2022-11-06 12:57:46 +0100

    1

---
+++ src/main/java/pl/sda/j133/pliki/pawel/zadania/odczyt/Zadanie4.java
@@ -0,0 +1,41 @@
+package pl.sda.j133.pliki.pawel.zadania.odczyt;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Paweł Recław, AmeN
+ * @project J133-Pliki
+ * @created 06.11.2022
+ */
+public class Zadanie4 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader czytacz = new BufferedReader(
+                    new FileReader("output_4.txt"));
+
+            int ilośćOsób = 0;
+            String liniaTekstu;
+            do {
+                liniaTekstu = czytacz.readLine();
+                if (liniaTekstu != null) {
+                    ilośćOsób++;
+                    String[] slowa = liniaTekstu.split(";");
+                    System.out.println("" + slowa[0] + " -> " + slowa[1]);
+
+                    // Integer.parseInt()
+                }
+
+            } while (liniaTekstu != null);
+            System.out.println("Ilość osób: " + ilośćOsób);
+
+            czytacz.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("O nie! Pliku nie ma!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
